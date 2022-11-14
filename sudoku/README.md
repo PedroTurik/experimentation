c35679228fbaa72bd6f019e05a068b8d48ea3654
PedroTurik <pedroturik@gmail.com>
2022-11-14 01:22:43 -0300

    sudoku

---
+++ sudoku/README.md
@@ -0,0 +1,7 @@
+Terminal Sudoku Solver using backtracking algorithm.
+
+The way to use it is to run sudoku_solver, then type the number you want to place at the index with an "_".
+
+Type the number and click enter to place it
+Type "r" and click enter to go back an index
+click Ctrl+D to get the answer, or just click enter until the and of the matrix
--- sudoku/sudoku_solve.py
+++ sudoku/sudoku_solve.py
@@ -20,44 +20,49 @@ def recur(board):
             board[y][x] = "."
         
 def sudoku_printer(board):
+    style = "\u001b[1m\u001b[47;1m\033[1m\033[96m"
+    reset = "\u001b[0m"
     giga_count = 0
     for _ in range(25):
-        print("-", end="")
-    print()        
+        print(style + "-", end="")
+    print(reset)        
     for row in board:
+        print(style, end="")
         print("|",end=" ")
         count = 0
         for n in row:
             print(n, end=" ")
             count += 1
-            if count == 3:
-                count = 0
-                print("|",end=" ")
+            if count%3==0:
+                if count == 9: print("|" + reset, end="")
+                else:
+                    print("|",end=" ")
         giga_count += 1
         print()
         if giga_count == 3:
             giga_count = 0
             for _ in range(25):
-                print("-", end="")
-            print()
+                print(style + "-", end="")
+            print(reset)
+    print(reset)
 
-
-if __name__=="__main__":
+def main():
     board = [
-    [".",".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".",".","."]
+        [".",".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."],
+        [".",".",".",".",".",".",".",".","."]
     ]
     try:
         y, x = 0, 0
         while True:
             if y == 8 and x == 9:
+                x = 8
                 raise EOFError
             if x > 8:
                 x = 0
@@ -69,25 +74,7 @@ if __name__=="__main__":
             print(chr(27) + "[2J")
             sudoku_printer(board)
             inp = input()
-            if inp == "^[[A":
-                board[y][x] = "."
-                if y > 0:
-                    y -= 1
-                else:
-                    continue
-            elif inp == "^[[C":
-                board[y][x] = "."
-                x += 1
-            elif inp == "^[[B":
-                board[y][x] = "."
-                if y < 8:
-                    y += 1
-                else:
-                    continue
-            elif inp == "^[[D":
-                board[y][x] = "."
-                x -= 1
-            elif not inp:
+            if not inp:
                 board[y][x] = "."
                 x += 1
             elif inp == "r":
@@ -99,6 +86,9 @@ if __name__=="__main__":
     except EOFError:
         board[y][x]="."
         print("solving.....")
-
     recur(board)
     sudoku_printer(board)
+
+if __name__=="__main__":
+    main()
+    
